9548b928c1557f6aea98e0fa642e2676afb08703
jilkinalexandr <jilkinalexandr@gmail.com>
2018-10-08 21:20:39 +0800

    поправил "Выйти": создал обработoutчик клика по кнопке выйти на клиенте, который эмитит событие log_out на socket.io

--- socket/index.js
+++ socket/index.js
@@ -4,14 +4,12 @@ var config = require('config');
 var connect = require('connect'); // npm i connect
 var async = require('async');
 var cookie = require('cookie');   // npm i cookie
-var cookieParser = require('cookie-parser');
 var sessionStore = require('lib/sessionStore');
 var HttpError = require('error').HttpError;
 var User = require('models/user').User;
 
 function loadSession(sid, callback) {
 
-    // sessionStore callback is not quite async-style!
     sessionStore.load(sid, function(err, session) { //load - умеет по sid находить объект сессии session в базе данных монго
         if (arguments.length == 0) {
             // no arguments => no session
@@ -20,7 +18,6 @@ function loadSession(sid, callback) {
             return callback(null, session);
         }
     });
-
 }
 
 function loadUser(session, callback) {
@@ -30,15 +27,12 @@ function loadUser(session, callback) {
         return callback(null, null);
     }
 
-    console.log("retrieving user " + session.user);
-
     User.findById(session.user, function(err, user) {
         if (err) return callback(err);
 
         if (!user) {
             return callback(null, null);
         }
-        console.log("user findbyId result: " + user);
         callback(null, user);
     });
 
@@ -51,97 +45,13 @@ module.exports = function(server) {
     // io.set('logger', log); // опция логгер позволяет указать свой объект в который будет осуществляться логгирование
 
 
-    // io.set('authorization', function(handshake, callback) {
-    //     handshake.fooo = 'svoistvo fooo';
-    //     async.waterfall([
-    //         function(callback) {
-    //             // сделать handshakeData.cookies - объектом с cookie
-    //             handshake.cookies = cookie.parse(handshake.headers.cookie || ''); // используем модуль cookie для извлечения куки из handshake
-    //             console.log("handshake.cookies  "+JSON.stringify(handshake.cookies));
-    //             var sidCookie = handshake.cookies[config.get('session:key')]; // получаем куку с идентификатором сессии, но этот идент-р подписан
-    //             var sid = connect.utils.parseSignedCookie(sidCookie, config.get('session:secret')); // используя метод connect-a очищаем идент-р от подписи
-    //
-    //             loadSession(sid, callback);  // loadSession это всего лишь обертка для красоты кода и для того, что бы callback вызвался
-    //             // с аргументами в соответсвии со стандартом, т.к. sessionStore.load(sid, function(err, session) {...} вызывает свой
-    //             // коллбек не в соответсвие с стандартом (43 видео 10-00)
-    //         },
-    //
-    //
-    //         // function(callback) {
-    //         //     var secret = config.get('session:secret');
-    //         //     var sessionKey = config.get('session:key');
-    //         //
-    //         //     console.log(handshake.signedCookies + ' 123'); // undefined
-    //         //
-    //         //     var cookieParser = express.cookieParser(secret);
-    //         //     cookieParser(handshake, {}, function (err) {
-    //         //         if (err) return callback(err);
-    //         //
-    //         //         var sid = handshake.signedCookies[sessionKey]; // y handshake появилось поле 'signedCookies'
-    //         //
-    //         //         loadSession(sid, callback);
-    //         //     });
-    //         // },
-    //         function(session, callback) {
-    //
-    //             if (!session) {
-    //                 callback(new HttpError(401, "No session"));
-    //             }
-    //
-    //             handshake.session = session;
-    //             loadUser(session, callback);
-    //         },
-    //         function(user, callback) {
-    //             if (!user) {
-    //                 callback(new HttpError(403, "Anonymous session may not connect"));
-    //             }
-    //
-    //             handshake.user = user;
-    //             callback(null);
-    //         }
-    //
-    //     ], function(err) {
-    //         if (!err) {
-    //             console.log("ошибки нет при создании сокета");
-    //
-    //             return callback(null, true); // ошибка - null, можно подключиться - true
-    //         }
-    //
-    //         if (err instanceof HttpError) {
-    //             return callback(null, false); // ошибку обработает экспресс?
-    //         }
-    //
-    //         callback(err); // если непонятная ошибка, то socket.io с ней как-то разберется
-    //     });
-    //     callback(null, true);
-    // });
-
     io.use(function(socket, next){
-        // console.log("probuem zapisat svoystvo fooo");
-        //
-        // socket.handshake.fooo = 'svoistvo fooo';
-        // next();
-        async.waterfall([
-            // function(callback) {
-            //     // сделать handshakeData.cookies - объектом с cookie
-            //     console.log('opa opa1');
-            //     socket.handshake.cookies = cookie.parse(handshake.headers.cookie || ''); // используем модуль cookie для извлечения куки из handshake
-            //     // console.log("handshake.cookies  "+JSON.stringify(handshake.cookies));
-            //     console.log('opa opa2');
-            //     var sidCookie = socket.handshake.cookies[config.get('session:key')]; // получаем куку с идентификатором сессии, но этот идент-р подписан
-            //     var sid = connect.utils.parseSignedCookie(sidCookie, config.get('session:secret')); // используя метод connect-a очищаем идент-р от подписи
-            //
-            //     loadSession(sid, callback);  // loadSession это всего лишь обертка для красоты кода и для того, что бы callback вызвался
-            //     // с аргументами в соответсвии со стандартом, т.к. sessionStore.load(sid, function(err, session) {...} вызывает свой
-            //     // коллбек не в соответсвие с стандартом (43 видео 10-00)
-            // },
 
+        async.waterfall([
 
             function(callback) {
                 var secret = config.get('session:secret');
                 var sessionKey = config.get('session:key');
-
-                console.log(socket.handshake.signedCookies + ' 123'); // undefined
 
                 var cookieParser = express.cookieParser(secret);
                 cookieParser(socket.handshake, {}, function (err) {
@@ -173,55 +83,28 @@ module.exports = function(server) {
         ], function(err) {
             if (!err) {
                 console.log("ошибки нет при создании сокета");
-                next();
+                return next();
             }
 
             if (err instanceof HttpError) {
-                console.log(err); // ошибку обработает экспресс?
+                console.log(err);
                 next(err);
             }
 
-            console.log("index.js - ne HttpError"); // если непонятная ошибка, то socket.io с ней как-то разберется
+            console.log("index.js: ne HttpError"); // если непонятная ошибка, то socket.io с ней как-то разберется
             next(err);
         });
 
     });
 
-    io.on('session:reload', function(sid) {
-        var clients = io.sockets.clients(); // получаем все сокеты
 
-        clients.forEach(function(client) { // в client лежит конкретный сокет
-            if (client.handshake.session.id != sid) return;
-
-            loadSession(sid, function(err, session) {
-                if (err) {
-                    client.emit("error", "server error");
-                    client.disconnect();
-                    return;
-                }
-
-                if (!session) {
-                    client.emit("logout"); // запустит на КЛИЕНТЕ событие логаут
-                    client.disconnect();
-                    return;
-                }
-
-                client.handshake.session = session;
-            });
-
-        });
-
-    });
-
-    io.on('connection', function(socket) { // socket это объект, который связан с данным конкретным клиентом
-        console.log("index.js on connection " + JSON.stringify(socket.handshake));
-        console.log("index.js on connection " + socket.handshake.fooo);
+    io.on('connection', function(socket) {
 
         var username = socket.handshake.user.username;
 
         socket.broadcast.emit('join', username);
 
-        socket.on('message', function(text, cb) { // получаем сообщение от клиента, безымянная функция обрабатывает пришедшие данные. .emit - генерирует события, on. - слушает.
+        socket.on('message', function(text, cb) {
             socket.broadcast.emit('message', username, text); // отправит сообщение всем соединениям кроме данного
             cb(); // та самая функция, которая запускает путем передачи любых данных на клиенте третий аргумент emit-a "message" (функцию)
         });
@@ -229,6 +112,12 @@ module.exports = function(server) {
         socket.on('disconnect', function() {
             socket.broadcast.emit('leave', username);
         });
+
+        socket.on('log_out', function() {
+            socket.handshake.session.destroy(function (err) {});
+            socket.disconnect();
+        });
+
 
     });
 
