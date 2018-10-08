ceb4162d13957de277e382f191e2355ae571ed50
jilkinalexandr <jilkinalexandr@gmail.com>
2018-10-08 23:58:30 +0800

    подчистил лишний файл в роутере, поправил файл сокета io

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ module.exports = function (app) {
 
     app.get('/login', require('./login').get);
     app.post('/login', require('./login').post);
-    app.post('/logout', require('./logout').post);
 
     app.get('/chat', checkAuth, require('./chat').get);  // app.get позволяет запускать миддлверы, например, checkAuth
 
